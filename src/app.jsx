e6865e96031fa265733f0053b93a36de1fddc005
Reina-Longoria <reina-longoria@macbook-pro.lan>
2019-04-18 21:29:38 -0400

    move file list state up into app. Pass the file list to the list component via props

--- src/app.jsx
+++ src/app.jsx
@@ -8,10 +8,64 @@ var sessionAudioFilesFolder = dialog.showOpenDialog({
 });
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [],
+      sessionAudioFilesFolder: sessionAudioFilesFolder
+    }
+  }
+
+  watchForFiles(){
+    var chokidar = require('chokidar');
+
+    var watcher = chokidar.watch(this.state.sessionAudioFilesFolder, {
+      ignored: /(^|[\/\\])\../,
+      persistent: true
+    });
+    function checkIfThePathIsInTheList(arr, path) {
+      return  arr.some(function(arrVal){
+        return arrVal.event === path
+      })
+    }
+    watcher
+    .on('add', (path, event) => {
+        if (checkIfThePathIsInTheList(this.state.list, path) != true){
+          this.setState(state=>{
+            const list = state.list.concat({event:path}).sort(function(a,b){
+              return b.event > a.event
+            })
+            return {list}
+          })
+        }
+    })
+    .on('unlink', (deletedFile) => {
+      var updatedArray = [...this.state.list];
+
+      function theLocationOfTheDeletedFile(fileInList) {
+        return fileInList.event === deletedFile;
+      }
+
+      var indexOfOutgoingFile = this.state.list.findIndex(theLocationOfTheDeletedFile)
+
+      updatedArray.splice(indexOfOutgoingFile, 1);
+      this.setState({list: updatedArray});
+
+    })
+  }
+
   render() {
-    return (<div>
+    return (
+      <div>
+      {this.watchForFiles()}
       <h2>{sessionAudioFilesFolder}</h2>
-      <List sessionAudioFilesFolder={sessionAudioFilesFolder}/>
+      <List sessionAudioFilesFolder={sessionAudioFilesFolder}
+            list={this.state.list}
+      />
     </div>);
   }
 }
+
+
+/*here's where I started getting crazy*/
--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,69 +1,14 @@
 import ListItem from './listItem'
 import React, { Component } from 'react';
 
-// const {dialog} = require('electron').remote;
-//
-// var sessionAudioFilesFolder = dialog.showOpenDialog({
-//     properties: ['openDirectory']
-// });
-
+/*here's where I started getting crazy*/
 class List extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      list: [],
-      sessionAudioFilesFolder: this.props.sessionAudioFilesFolder
-    }
-  }
-
-  watchForFiles(){
-    var chokidar = require('chokidar');
-
-    var watcher = chokidar.watch(this.state.sessionAudioFilesFolder, {
-      ignored: /(^|[\/\\])\../,
-      persistent: true
-    });
-    function checkIfThePathIsInTheList(arr, path) {
-      return  arr.some(function(arrVal){
-        return arrVal.event === path
-      })
-    }
-    watcher
-    .on('add', (path, event) => {
-        if (checkIfThePathIsInTheList(this.state.list, path) != true){
-          this.setState(state=>{
-            const list = state.list.concat({event:path}).sort(function(a,b){
-              return b.event > a.event
-            })
-            return {list}
-          })
-        }
-    })
-    .on('unlink', (deletedFile) => {
-      var updatedArray = [...this.state.list];
-
-      function theLocationOfTheDeletedFile(fileInList) {
-        return fileInList.event === deletedFile;
-      }
-
-      var indexOfOutgoingFile = this.state.list.findIndex(theLocationOfTheDeletedFile)
-
-      updatedArray.splice(indexOfOutgoingFile, 1);
-      this.setState({list: updatedArray});
-
-    })
-  }
-
-
 
   render() {
     return (
       <div>
         <ul>
-          {this.watchForFiles()}
-
-          {this.state.list.map( (fileEvent,index) => (
+          {this.props.list.map( (fileEvent,index) => (
             <li key={index}>{fileEvent.event}</li>
           ))}
         </ul>
